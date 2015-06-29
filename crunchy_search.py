37f6d4003779af705dd4f373aa532f0d9b64a141
Ethan Chan <metakirby5@gmail.com>
2015-06-29 03:39:29 -0700

    better searching(?), change api, handle no connection

--- crunchy_search.py
+++ crunchy_search.py
@@ -9,27 +9,43 @@ class SearchError(Exception):
   pass
 
 def search(query):
-  # Encode the query
-  query = quote(query.replace(' ', '+'))
+  """
+  Returns the first result of a Crunchyroll search
+  as a dict with keys of:
 
+  {
+    'url': str,
+    'series': str,
+    'ep_name': str,
+    'ep_no': int
+  }
+
+  Raises SearchError if no results found.
+  """
   # Request the url
-  r = requests.get(SEARCH_URL % query)
-  return r.text
-
-def parse(body):
-  soup = BeautifulSoup(body)
-
-  # Return the url of the first result
   try:
-    return soup.find(id=RESULT_ID).a['href']
+    r = requests.get(SEARCH_URL % quote(query))
+  except requests.exceptions.ConnectionError:
+    raise SearchError('No connection')
+
+  soup = BeautifulSoup(r.text)
+
+  # Return the first result
+  try:
+    result = soup.find(id=RESULT_ID).a
+    return {
+      'url':      result['href'],
+      'series':   result.find(class_='series').string,
+      'ep_name':  result.find(class_='name').string,
+      'ep_no':    int(result.find(class_='ordernum').string.split()[-1]),
+    }
   except TypeError:
-    raise SearchError('No results found.')
+    raise SearchError('No results found')
 
 def main():
   query = raw_input()
-  body = search(query)
-  url = parse(body)
-  print(url)
+  result = search(query)
+  print(result['url'])
 
 if __name__ == '__main__':
   main()
